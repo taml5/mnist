3d04ce20bb527f16f77cea9e14b5ffa678ea55c4
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-07-26 09:07:31 +0800

    Added tooltips

--- frontend/src/components/ControlMenu.tsx
+++ frontend/src/components/ControlMenu.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction, useState } from "react";
-import { Button, Slider } from "@mui/material";
+import { Button, Slider, Tooltip } from "@mui/material";
 import "./ControlMenu.css";
 
 interface IControlMenu {
@@ -13,102 +13,118 @@ interface IControlMenu {
     setImgSrc: Dispatch<SetStateAction<string>>
 }
 
+// TODO: update the colour scheme
 function ControlMenu(prop: IControlMenu) {
     const [training, setTraining] = useState<boolean>(false);
     const [evaluating, setEvaluating] = useState<boolean>(false);
-
-    const [trainingText, setTrainingText] = useState<string>("Train");
     const [epochs, setEpochs] = useState<number>(1);
     const [batchSize, setBatchSize] = useState<number>(5);
     const [learningRate, setLearningRate] = useState<number>(3 * 20);
 
     return (<div className="control-menu">
         <div className="buttons">
-            <Button 
-                className="Evaluate" 
-                variant="contained" 
-                size="large"
-                disabled={evaluating}
-                sx= {{
-                    width: 200
-                }}
-                onClick={async () => {
-                    setEvaluating(true);
-                    try {
-                        const response = await fetch(import.meta.env.VITE_API_URL + '/evaluate');
-                        if (response) {
-                            const data = await response.json();
-                            prop.setPrevRate(prop.currRate);
-                            prop.setRate(data.rate);
-                        }
-                    } catch (error) {
-                        console.log(error);
-                    }
-                    setEvaluating(false);
-                }}
+            <Tooltip 
+                title="Evaluate the number of successful predictions over the entire testing image set."
+                placement="left"
+                enterDelay={1000}
             >
-                Evaluate
-            </Button>
-            <Button 
-                className="Test" 
-                variant="contained" 
-                size="large"
-                sx= {{
-                    width: 200
-                }}
-                onClick={async () => {
-                    try {
-                        const response = await fetch(import.meta.env.VITE_API_URL + '/test');
-                        if (response) {
-                            const data = await response.json();
-                            prop.setPred(data.guess);
-                            prop.setActual(data.answer);
-                            prop.setActivations(data.activations);
-                            prop.setImgSrc(data.image);
-                        }
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }}
-            >
-                Test
-            </Button>
-            <Button 
-                className="Train" 
-                variant="contained" 
-                size="large" 
-                disabled={training}
-                sx= {{
-                    width: 200
-                }}
-                onClick={async () => {
-                    setTraining(true);
-                    setTrainingText("Training...")
-                    try {
-                        await fetch(import.meta.env.VITE_API_URL + '/train', {
-                            method: "POST",
-                            body: JSON.stringify({
-                                "epochs": epochs,
-                                "batch_size": 2 ** batchSize,
-                                "learning_rate": learningRate / 20
-                            }),
-                            headers: {
-                                "Content-type": "application/json; charset=UTF-8"
+                <Button 
+                    className="Evaluate" 
+                    variant="contained" 
+                    size="large"
+                    disabled={evaluating}
+                    sx= {{
+                        width: 200
+                    }}
+                    onClick={async () => {
+                        setEvaluating(true);
+                        try {
+                            const response = await fetch(import.meta.env.VITE_API_URL + '/evaluate');
+                            if (response) {
+                                const data = await response.json();
+                                prop.setPrevRate(prop.currRate);
+                                prop.setRate(data.rate);
                             }
-                        });
-                    } catch (error) {
-                        console.log(error);
-                    }
-                    setTraining(false);
-                    setTrainingText("Train")
-                }}
+                        } catch (error) {
+                            console.log(error);
+                        }
+                        setEvaluating(false);
+                    }}
+                >
+                    Evaluate
+                </Button>
+            </Tooltip>
+            <Tooltip 
+                title="Test the neural network on a single random input picture."
+                enterDelay={1000}
             >
-                {trainingText}
-            </Button>
+                <Button 
+                    className="Test" 
+                    variant="contained" 
+                    size="large"
+                    sx= {{
+                        width: 200
+                    }}
+                    onClick={async () => {
+                        try {
+                            const response = await fetch(import.meta.env.VITE_API_URL + '/test');
+                            if (response) {
+                                const data = await response.json();
+                                prop.setPred(data.guess);
+                                prop.setActual(data.answer);
+                                prop.setActivations(data.activations);
+                                prop.setImgSrc(data.image);
+                            }
+                        } catch (error) {
+                            console.log(error);
+                        }
+                    }}
+                >
+                    Test
+                </Button>
+            </Tooltip>
+            <Tooltip 
+                title="Train the neural network with the below hyperparameters. This could take a while."
+                placement="right"
+                enterDelay={1000}
+            >
+                <Button 
+                    className="Train" 
+                    variant="contained" 
+                    size="large" 
+                    disabled={training}
+                    sx= {{
+                        width: 200
+                    }}
+                    onClick={async () => {
+                        setTraining(true);
+                        try {
+                            await fetch(import.meta.env.VITE_API_URL + '/train', {
+                                method: "POST",
+                                body: JSON.stringify({
+                                    "epochs": epochs,
+                                    "batch_size": 2 ** batchSize,
+                                    "learning_rate": learningRate / 20
+                                }),
+                                headers: {
+                                    "Content-type": "application/json; charset=UTF-8"
+                                }
+                            });
+                        } catch (error) {
+                            console.log(error);
+                        }
+                        setTraining(false);
+                    }}
+                >
+                    {training ? "Training..." : "Train"}
+                </Button>
+            </Tooltip>
         </div>
         <div className="sliders">
             <div className="slider">
-                <p>Epochs</p>
+                <Tooltip title="How many times the neural network trains on the entire training dataset." placement="left">
+                    <p>Epochs</p>
+                </Tooltip>
                 <Slider
                     className="mui-slider"
                     aria-label="Epochs"
@@ -125,7 +141,9 @@ function ControlMenu(prop: IControlMenu) {
                 />
             </div>
             <div className="slider">
-                <p>Batch Size</p>
+                <Tooltip title="The number of samples that are tested before the weights are updated in the network." placement="left">
+                    <p>Batch Size</p>
+                </Tooltip>
                 <Slider
                     className="mui-slider"
                     aria-label="Batch Size"
@@ -143,7 +161,9 @@ function ControlMenu(prop: IControlMenu) {
                 />
             </div>
             <div className="slider">
-                <p>Learning Rate</p>
+                <Tooltip title="How much the network changes in response to the error in training a batch." placement="left">
+                    <p>Learning Rate</p>
+                </Tooltip>
                 <Slider
                     className="mui-slider"
                     aria-label="Learning Rate"
